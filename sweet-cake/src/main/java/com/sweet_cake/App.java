038d63f21be71f620943412ac9ff5ffb542d8a07
Danijel Blagojevic <danijel@mybookingpal.com>
2016-11-07 19:54:55 +0100

    Devel: Add initial project structure.

---
+++ sweet-cake/src/main/java/com/sweet_cake/App.java
@@ -0,0 +1,13 @@
+package com.sweet_cake;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
